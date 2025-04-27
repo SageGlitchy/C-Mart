526b4985dd961025f8bbf6c4994387e9e70e6bbe
Madhav Mangla <sageglitchy@gmail.com>
2025-04-27 23:57:42 +0530

    Add frontend/src/pages/Home.jsx 1745778475366

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Home;
